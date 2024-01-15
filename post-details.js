1bdf27414c0616e5f3043fe3e2b3bc44278302ca
Yura <yuras92@ukr.net>
2024-01-15 02:35:06 +0200

    refactoring

--- post-details.js
+++ post-details.js
@@ -6,44 +6,42 @@ const postUrl = `https://jsonplaceholder.typicode.com/posts/${postId}`;
 const commentsUrl = `https://jsonplaceholder.typicode.com/posts/${postId}/comments`;
 const bckButton = document.getElementById('bck-to-usr');
 
-function fetchData(url) {
-
-    return fetch(url)
-        .then(response => response.json());
-}
-
 Promise.all([fetchData(postUrl), fetchData(commentsUrl)])
     .then(([post, comments]) => {
-        function flatter(obj, container, className) {
-            for (const key in obj) {
-                const p = document.createElement('p');
-                p.classList.add(className);
-                if (typeof obj[key] === 'object') {
-                    flatter(obj[key], container, className);
-                } else {
-                    p.innerText = `${key}: ${obj[key]}`;
-                    container.appendChild(p);
-                }
-            }
-        }
+        bckButton.onclick = ()=> {window.location=`user-details.html?id=${post.userId}`}
 
-        bckButton.href = 'user-details.html?id='+post.userId
-
-        flatter(post, infoContainer, 'postInfo');
-
-        comments.forEach(comment => {
-            const commentBody = document.createElement('div');
-            commentBody.classList.add('box')
-            commentBody.innerHTML = '';
-            commentBody.innerText = comment.body;
-
-            commentsContainer.appendChild(commentBody);
-        });
+        flatter(post);
+        createCommentBlock(comments)
     })
     .catch(error => {
         console.error('Error fetching data:', error);
     });
 
+function fetchData(url) {
+    return fetch(url)
+        .then(response => response.json());
+}
 
+function flatter(obj) {
+    for (const key in obj) {
+        const p = document.createElement('p');
+        p.classList.add('postInfo');
+        if (typeof obj[key] === 'object') {
+            flatter(obj[key]);
+        } else {
+            p.innerText = `${key}: ${obj[key]}`;
+            infoContainer.appendChild(p);
+        }
+    }
+}
 
+function createCommentBlock(comments) {
+    comments.forEach(comment => {
+        const commentBody = document.createElement('div');
+        commentBody.classList.add('box')
+        commentBody.innerHTML = '';
+        commentBody.innerText = comment.body;
+        commentsContainer.appendChild(commentBody);
+    });
+}
 
--- user-details.js
+++ user-details.js
@@ -7,29 +7,14 @@ const infoContainer = document.getElementById('infoContainer');
 const postsContainer = document.getElementById('postsContainer');
 const h2 = document.createElement("h2");
 
-
 fetch(userInfoUrl)
     .then(response => response.json())
     .then(user => {
-        function flatter(obj) {
-            for (const key in obj) {
-                const p = document.createElement('p');
-                p.classList.add('userInfo')
-                if (typeof obj[key] === 'object') {
-                    flatter(obj[key]);
-                } else {
-                    p.innerText = `${key}: ${obj[key]}`;
-                    infoContainer.appendChild(p);
-                }
-            }
-        }
-
         flatter(user);
     })
     .catch(error => {
         console.error('Error fetching user info:', error);
     });
-
 function getUserPosts() {
     fetch(userPostsUrl)
         .then(r => r.json())
@@ -37,18 +22,8 @@ function getUserPosts() {
             postsContainer.innerHTML = '';
             h2.innerHTML = `User's ${userId} Posts`
             infoContainer.after(h2)
-            posts.forEach(post => {
 
-                const postBlock = document.createElement('div');
-                const a = document.createElement('a');
-
-                postBlock.classList.add('box');
-                postBlock.innerText = post.title;
-                a.href = 'post-details.html?id='+post.id
-
-                a.appendChild(postBlock)
-                postsContainer.appendChild(a);
-            });
+            createPostBlock(posts)
             postsContainer.scrollIntoView({behavior: 'smooth'});
         })
         .catch(error => {
@@ -58,6 +33,35 @@ function getUserPosts() {
 
 button.addEventListener('click', getUserPosts);
 
+function flatter(obj) {
+    for (const key in obj) {
+        const p = document.createElement('p');
+        p.classList.add('userInfo')
+        if (typeof obj[key] === 'object') {
+            flatter(obj[key]);
+        } else {
+            p.innerText = `${key}: ${obj[key]}`;
+            infoContainer.appendChild(p);
+        }
+    }
+}
+
+function createPostBlock(posts) {
+    posts.forEach(post => {
+        const postBlock = document.createElement('div');
+        postBlock.classList.add('box');
+        postBlock.innerText = post.title;
+
+        postBlock.onclick = ()=>{
+            window.location = 'post-details.html?id='+post.id
+        }
+        postsContainer.appendChild(postBlock);
+    });
+}
+
+function goBackToUsers() {
+    window.location = 'index.html'
+}
 
 
 
